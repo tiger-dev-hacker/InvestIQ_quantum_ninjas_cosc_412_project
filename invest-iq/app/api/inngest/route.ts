7f5354a9ecc4f3dadbff297da587b8dd2e0532c8
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-11-23 18:04:18 -0500

    Fixed auth changes

--- invest-iq/app/api/inngest/route.ts
+++
@@ -1,8 +0,0 @@
-import {serve} from "inngest/next";
-import {inngest} from "@/lib/inngest/client";
-import {sendSignUpEmail} from "@/lib/inngest/functions";
-
-export const { GET, POST, PUT } = serve({
-    client: inngest,
-    functions: [sendSignUpEmail],
-})
